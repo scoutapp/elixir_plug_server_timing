6be7c661c5e9b627ae4ab37357eb6daf0aca4046
Mitchell Henke <mitchh23@gmail.com>
2018-03-06 16:27:32 -0600

    remove test

--- test/plug_server_timing_test.exs
+++ test/plug_server_timing_test.exs
@@ -1,19 +1,4 @@
 defmodule PlugServerTimingTest do
   use ExUnit.Case, async: true
-  use Plug.Test
   doctest PlugServerTiming
-
-  defp call(conn, opts) do
-    PlugServerTiming.Plug.call(conn, PlugServerTiming.Plug.init(opts))
-  end
-
-  test "sets server-timing header" do
-    conn =
-      call(conn(:get, "/"), [])
-      |> send_resp(200, "")
-
-    [timing] = get_resp_header(conn, "server-timing")
-
-    assert Regex.match?(~r/^total;dur=[\d\.]+$/, timing)
-  end
 end
